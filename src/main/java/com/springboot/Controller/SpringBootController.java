fbf8d65846e28360198b39815a51857b6ff899b4
shicaixin <scxin205@163.com>
2020-05-21 13:53:44 +0800

    test

--- src/main/java/com/springboot/Controller/SpringBootController.java
+++ src/main/java/com/springboot/Controller/SpringBootController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class SpringBootController {
 	@RequestMapping("")
 	public String hello(){
-		return "hello springboot";
+		return "hello git";
 	}
 }
